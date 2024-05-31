16dc01e887cc8430af57c2593f9e32cbc96f91d6
Juya <juyaamirm@gmail.com>
2024-05-31 16:09:26 +0200

    deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
-
-
+import { useEffect, useState } from "react"
+import axios from 'axios'
 function App() {
+  const [data, setData] = useState([]);
+    const getData = async() => {
+      try {
+        const response = await axios.get(`https://geo.ipify.org/api/v2/country?apiKey=${import.meta.env.VITE_KEY}&ipAddress=${import.meta.env.VITE_IP}`);
+        console.log(response.data);
+        setData(response.data);
+      } catch (error) {
+        console.error("Error", error)
+      }
+    }
+   
 
+    useEffect(() => {
+      getData();
+    }, []);
 
   return (
     <>
-      Hello
+      <p>IP Address: <span className="bg-yellow-200 text-red-600">{data.ip}</span></p> 
+      <p>ISP: <span className="bg-yellow-200 text-red-600">{data.isp}</span></p>
+      <p>Country: <span className="bg-yellow-200 text-red-600">{data.location.country}</span></p>
+      <p>Region: <span className="bg-yellow-200 text-red-600">{data.location.region}</span></p>
     </>
   )
 }
